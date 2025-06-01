761202898b2eb270df5a1ba1f66c4d1865e1f89c
samblechman <seb203@pitt.edu>
2025-05-31 22:16:46 -0400

    Edited plotting functions

--- R_scripts/007_IdentifySigGenes.R
+++ R_scripts/007_IdentifySigGenes.R
@@ -1,4 +1,5 @@
 # Load necessary data.
+setwd('~/Desktop/SequenceToMutations/')
 load(file = 'RdataFiles/ancestor_gbk_dataframes.Rdata') # all ancestor assmebly gene and gene product information
 load(file = 'RdataFiles/mutations_list.Rdata') # all evolved lineage mutations tables
 
@@ -71,37 +72,42 @@ for (i in seq_len(nrow(sig_genes))) {
 rm(i)
 
 # Add columns to data.frame for transformed p-values and odds ratios*
-sig_genes$pval <- -log10(pvals)
+sig_genes$pval <- pvals
 sig_genes$OR <- oddsRs
 
 
 # Show top 6 genes enriched in VAN-exposed group.
-head(sig_genes[order(log(sig_genes$OR), decreasing=T), ])
+head(sig_genes[order(sig_genes$pval), ])
 
 # Bonferroni-corrected p-value=0.01 threshold.
-pval_cutoff <- -log10(0.01 / nrow(sig_genes))
+pval_cutoff <- 0.01 / nrow(sig_genes)
 
 # Plotting variables.
-col_vec <- '#bbbbbb'
+col_vec <- c('ns' = '#bbbbbb', 'sig' = '#000000')
 pch_vec <- 16
 pcex <- 0.75
 tcex <- 1
 
 # Function to plot proportion of 
 mutsXY <- function(df) {
-   plot(x = df$VAN.exposed / num_lins,
-        y = df$VAN.unexposed / num_lins,
+   plot(NA,
         xlim = c(0, 1),
         ylim = c(0, 1),
-        xpd = NA,
         xaxt = 'n', xaxs = 'i', xlab = '',
-        yaxt = 'n', yaxs = 'i', ylab = '',
-        pch = pch_vec,
-        col = col_vec,
-        bg = col_vec,
-        cex = pcex)
+        yaxt = 'n', yaxs = 'i', ylab = '')
+   abline(a=0, b=1, lty=2)
+   
+   points(x = df$VAN.exposed / num_lins,
+          y = df$VAN.unexposed / num_lins,
+          xpd = NA,
+          pch = pch_vec,
+          col = ifelse(df$pval < pval_cutoff, col_vec['sig'], col_vec['ns']),
+          bg = ifelse(df$pval < pval_cutoff, col_vec['sig'], col_vec['ns']),
+          cex = pcex)
+   
    title(xlab = 'proportion in VAN-exposed', line=1.75)
-   title(ylab = 'proportion in VAN-unexposed', line=1.75)
+   title(ylab = 'proportion in VAN-unexposed', line=2.25)
+   
    axis(side = 1,
         at = seq(0, 1, 0.2),
         labels = seq(0, 1, .2))
@@ -109,78 +115,45 @@ mutsXY <- function(df) {
         las = 1,
         at = seq(0, 1, 0.2),
         labels = seq(0, 1, .2))
-   abline(a=0, b=1)
-}
-
-addLegend <- function() {
-   x <- -0.63
-   y <- 1.01
-   xshift <- rep(0.034, 2)
-   yshift <- c(0.05, 0.094)
-   text(x = x + xshift + 0.025,
-        y = y - yshift,
-        labels = c('         operon genes', 
-                   'other genes'),
-        adj = c(0, 0.5),
-        cex = tcex,
-        xpd = NA)
-   
-   points(x = x + xshift, 
-          y = y - yshift - 0.0025,
-          pch = c(17, 19),
-          cex = pcex+0.1,
-          col = c('black', graycol),
-          xpd = NA)
-   
-   seg_y_pos <- 0.874
-   segments(x0 = c(x, x),
-            x1 = c(-0.202, x),
-            y0 = c(seg_y_pos, seg_y_pos),
-            y1 = c(seg_y_pos, 1),
-            xpd = NA)
-   
-   text(x = x + 0.102,
-        y = y - yshift[1],
-        cex = tcex,
-        labels = 'vanA',
-        xpd = NA,
-        font = 3)
 }
 
 volcano <- function(df, xlimit=6, ylim=c(-0.25,30)) {
-   plot(x = log(df$OR),
-        y = df$pval,
+   plot(NA, 
         xlim = c(-xlimit, xlimit),
         ylim = ylim,
         yaxs = 'i',
         xlab = '',
         ylab = '',
-        pch = pch_vec,
-        cex = pcex,
-        bg = col_vec,
-        yaxt = 'n',
-        col = col_vec)
+        yaxt = 'n')
+   abline(h = -log10(pval_cutoff), lty = 2)
+   abline(v = 0, lty=2)
+   
+   points(x = log(df$OR),
+          y = -log10(df$pval),
+          pch = pch_vec,
+          cex = pcex,
+          col = ifelse(df$pval < pval_cutoff, col_vec['sig'], col_vec['ns']),
+          bg = ifelse(df$pval < pval_cutoff, col_vec['sig'], col_vec['ns']))
    
    title(xlab = 'Vancomycin specificity score', line=1.75, xpd=NA)
    title(ylab = expression('-Log'[10]*'(p-value)'), line=1.75)
    
-   abline(h = pval_cutoff, lty = 2, lwd=0.8)
-   abline(v = 0, lty=3, lwd=1.2, col='#00000066')
-   
    axis(side = 2, at = seq(0, 25, 5), las = 1)
    
-   text(x = 6.35,
-        y = 5.4 + c(0.5, -0.5),
+   text(x = xlimit * 1.06,
+        y = -log10(pval_cutoff)*1.01 + c(0.5, -0.5),
         adj = c(1, 0.5),
         cex = tcex,
         labels = c('Bonferonni-corrected', 'p-value cutoff'))
 }
 
+
 # Make plots
 {
-   pdf(file = 'Plots/significant_mutations.pdf', width=10, height=5)
+   pdf(file = 'Plots/significant_mutations.pdf', width=10.5, height=5.25)
    par(mfrow=c(1,2), tck=-0.015, mgp=c(1.5,0.5,0), mar=c(3,3,2,1))
    volcano(df=sig_genes)
+   legend('bottomright', col=col_vec, legend=names(col_vec), pch=16)
    mutsXY(df=sig_genes)
    dev.off()
 }
